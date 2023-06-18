5f9ef122c6dded15522bc5be31225a8ba4a988ca
Alfred Ortega <jusalfort.07@gmail.com>
2023-06-17 19:43:33 -0600

    fix(lesson-4): fix link

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'My API',
     description: 'Description'
   },
-  host: 'https://cse-341-ortega.onrender.com/contacts',
-  schemes: ['http']
+  host: 'cse-341-ortega.onrender.com/contacts',
+  schemes: ['https']
 };
 
 const outputFile = './swagger.json';
